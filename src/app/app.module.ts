dc6ea75778864a1baf0ba39f957289fd2a8fc7c7
KellenNjoroge <muthonkel@gmail.com>
2018-08-21 18:49:55 +0300

    commit works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { ProfileComponent } from './profile/profile.component';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {ProfilesService} from './profile-service/profiles.service';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { RouterModule, Routes } from '@angular/router';
+// Defining routes
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+  {path: 'search', component: SearchComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    ProfileFormComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProfilesService],
   bootstrap: [AppComponent]
